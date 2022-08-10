eeac1aade721f856786c5fb767441d0e6db877b2
KarlaCh15 <kvasquez@redefectiva.com>
2022-08-10 09:44:21 -0500

    update
    modificación texto App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React karla estuvo aqui
         </a>
       </header>
     </div>
